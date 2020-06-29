c6e1ea17b829ac83332b15728fac57bdcb231c95
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-06-29 18:17:35 +0800

    The sum of digits

--- Problems/The sum of digits/src/Main.java
+++ Problems/The sum of digits/src/Main.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
+        int x = Integer.parseInt(scanner.next());
+        int a = x / 100;
+        int b = x % 100;
+        b = b / 10;
+        int c = x % 10;
+        System.out.println(a+b+c);
     }
 }
