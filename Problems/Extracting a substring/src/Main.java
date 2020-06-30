187199b6829e56803e925bf6e2e71dcb2cfab32e
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-06-30 19:20:06 +0800

    Extracting a substring

--- Problems/Extracting a substring/src/Main.java
+++ Problems/Extracting a substring/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int start = Integer.parseInt(sc.next());
+        int end = Integer.parseInt(sc.next());
+        System.out.print(str.substring(start, ++end));
     }
 }
