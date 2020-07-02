5dd75191dfc8dd4007daf22cb992a7caa021d8b0
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-07-02 15:17:08 +0800

    The army of units

--- Problems/The army of units/src/Main.java
+++ Problems/The army of units/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        long a = Integer.parseInt(scanner.next());
+        String res;
+        if (a < 1) {
+            res = "no army";
+        } else if (a < 20) {
+            res = "pack";
+        } else if (a < 250) {
+            res = "throng";
+        } else if (a < 1000) {
+            res = "zounds";
+        } else {
+            res = "legion";
+        }
+        System.out.print(res);
     }
 }
